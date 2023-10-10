b779c2e29f9aebe66c97aa923b1a42c1f4f8f199
Muxammed Safarov <muxammed@Muxammeds-MacBook-Pro.local>
2023-10-10 14:19:26 +0400

    Sidebar is finish

--- dashboard/src/components/Sidebar/Sidebar.jsx
+++ dashboard/src/components/Sidebar/Sidebar.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import "./Sidebar.scss";
 import { FaBookMedical } from "@react-icons/all-files/fa/FaBookMedical";
+import { MdDashboard } from "@react-icons/all-files/md/MdDashboard";
+import { BsGraphUp } from "@react-icons/all-files/bs/BsGraphUp";
+import { BsPeople } from "@react-icons/all-files/bs/BsPeople";
+import { BsChevronDown } from "@react-icons/all-files/bs/BsChevronDown";
+import { BiBuildings } from "@react-icons/all-files/bi/BiBuildings";
 
 const Sidebar = () => {
   return (
@@ -15,22 +20,30 @@ const Sidebar = () => {
         </div>
         <div className="sidebar-navigation">
           <div className="sidebar-sections" id="dashboard">
+            <MdDashboard className="icons" />
             <p>Dashboard</p>
           </div>
           <div className="sidebar-sections" id="forecast">
+            <BsGraphUp className="icons" />
             <p>Forecast</p>
           </div>
           <div className="sidebar-sections" id="all-offices">
+            <BiBuildings className="icons" />
             <p>All Offices</p>
           </div>
           <div className="sidebar-sections" id="employees">
+            <BsPeople className="icons" />
             <p>Employees</p>
           </div>
         </div>
       </div>
       <div className="sidebar-footer">
-        <img src="" alt="" />
-        <div>My Profile</div>
+        <img
+          src="https://www.its.ac.id/international/wp-content/uploads/sites/66/2020/02/blank-profile-picture-973460_1280.jpg"
+          alt=""
+        />
+        <div>My profile</div>
+        <BsChevronDown className="icon" />
       </div>
     </div>
   );
